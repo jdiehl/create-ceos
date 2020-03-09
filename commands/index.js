96f14509152bb042ec8fbd5377dbdaca58a7cfc3
Jonathan Diehl <jonathan.diehl@gmail.com>
2020-03-09 15:29:49 +0100

    added migration creation

--- commands/index.js
+++ commands/index.js
@@ -1,9 +1,11 @@
 const server = require('./server')
 const ext = require('./ext')
 const job = require('./job')
+const migration = require('./migration')
 
 module.exports = plop => {
   server(plop)
   ext(plop)
   job(plop)
+  migration(plop)
 }
---
+++ commands/migration/index.js
@@ -0,0 +1,34 @@
+const { readdirSync } = require('fs')
+const { join, extname } = require('path')
+
+function templateAction(path, filename, data = {}, newName) {
+  return {
+    type: 'add',
+    path: join(process.cwd(), path, newName ? newName : filename),
+    templateFile: join(__dirname, 'templates', `${filename}.hbs`),
+    skipIfExists: true,
+    data
+  }
+}
+
+module.exports = plop => {
+  plop.setGenerator('migration', {
+    description: 'Create a migration',
+    prompts: [
+      {
+      type: 'input',
+      name: 'name',
+      message: 'Migration name',
+      validate: name => name.length > 0
+    }],
+    actions: ({ name }) => {
+      const files = readdirSync('migrations').filter(file => extname(file) === '.js').sort()
+      const id = (parseInt(files.lastItem, 10) || 0) + 1
+      const prefix = (id < 10 ? '0' : '') + id
+      return [
+        templateAction('migrations', 'index.js', {}, `${prefix}-${name}.js`)
+      ]
+    }
+  })
+
+}
